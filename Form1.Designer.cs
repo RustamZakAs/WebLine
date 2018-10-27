063d8c488c5faa18aae6cde731ed1adad1cc5646
Rustam Zak <rostik055@gmail.com>
2018-10-27 14:44:18 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.image_editing_101040_960_720;
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listView1.Location = new System.Drawing.Point(8, 12);
+            this.listView1.Margin = new System.Windows.Forms.Padding(0);
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listView1.Size = new System.Drawing.Size(261, 481);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(272, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(632, 481);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 503);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<TwoPoint> listTwoPoints = new List<TwoPoint>();
+        Point pointMause;
+        Random randomX = new Random();
+        Random randomY = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            pointMause = new Point(e.X, e.Y);
+
+            listTwoPoints.Clear();
+            for (int i = 0; i < 5; i++)
+            {
+                listTwoPoints.Add(new TwoPoint(new Point(randomX.Next(1, pictureBox1.Width),
+                    randomY.Next(1, pictureBox1.Height)), pointMause, Color.Black));
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //Pen pen = new Pen(Color.Black);
+
+            //Point[] points = new Point[] {
+            //    new Point { X = point1.X, Y = point1.Y },
+            //    new Point { X = point3.X, Y = point3.Y },
+            //    new Point { X = pointMause.X, Y = pointMause.Y },
+            //    new Point { X = point2.X, Y = point2.Y },
+            //    new Point { X = point4.X, Y = point4.Y }
+            //    };
+
+
+            //Ctrl - A
+
+            //Ctrl + KF
+
+            //F12
+
+
+            foreach (var item in listTwoPoints)
+            {
+                e.Graphics.DrawLine(new Pen(item.Color), item.X, item.Y);
+            }
+        }
+    }
+
+    class TwoPoint
+    {
+        public Point X { get; set; }
+        public Point Y { get; set; }
+        public Color Color { get; set; }
+
+        public TwoPoint(Point x, Point y, Color color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+    }
+}
